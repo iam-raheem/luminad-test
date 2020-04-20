961098e42f5d43a48b6c63ca5a2bdf3ad35848aa
Raheem Shaik <raheem.shaik@fortunapix.com>
2020-04-20 18:58:06 +0530

    testing done again

--- bullet.rb
+++ bullet.rb
@@ -1,16 +1,8 @@
-# heads = [
-#     { id: 1, title: "heading1", heading_level: 0 },
-#     { id: 2, title: "heading2", heading_level: 2 },
-#     { id: 3, title: "heading3", heading_level: 1 },
-#     { id: 4, title: "heading4", heading_level: 1 }
-# ]
-
 heads = [
     { id: 1, title: "heading1", heading_level: 0 },
-    { id: 2, title: "heading2", heading_level: 3 },
-    { id: 3, title: "heading3", heading_level: 4 },
-    { id: 4, title: "heading4", heading_level: 1 },
-    { id: 5, title: "heading5", heading_level: 0 }
+    { id: 2, title: "heading2", heading_level: 2 },
+    { id: 3, title: "heading3", heading_level: 1 },
+    { id: 4, title: "heading4", heading_level: 1 }
 ]
 
 heads.each_with_index do |h, i|
@@ -18,15 +10,18 @@ heads.each_with_index do |h, i|
     level = h[:heading_level]+1
     arr = []
     n = level
+
+    arr << 1 if i == 0
+
     while n > 0
-        arr << 1
+        arr << 1 if h[:heading_level] > 0
         if pnt > 0
-            arr << pnt+1 
+            arr << pnt+1
             break
         end
         n = n-1
     end
 
     ar_str = arr.join(".")
-    puts " "*level+"#{ar_str}.#{h[:title]}"
+    puts " "*level+"#{ar_str}.#{h[:title].capitalize}"
 end
