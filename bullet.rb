225cdbcea9f23fbbca2132bc2807f085f689906d
Raheem Shaik <shaikraheem141@gmail.com>
2020-04-20 18:00:29 +0530

    Add files via upload

--- bullet.rb
+++ bullet.rb
@@ -5,17 +5,20 @@ heads = [
     { id: 4, title: "heading4", heading_level: 1 }
 ]
 
-heads.each do |h|
+heads.each_with_index do |h, i|
+    pnt = heads[0..i].select {|h| h[:heading_level] == 1 }.count
     level = h[:heading_level]+1
-
     arr = []
     n = level
     while n > 0
         arr << 1
+        if pnt > 0
+            arr << pnt+1 
+            break
+        end
         n = n-1
     end
 
     ar_str = arr.join(".")
-    # puts level
     puts " "*level+"#{ar_str}.#{h[:title]}"
 end
