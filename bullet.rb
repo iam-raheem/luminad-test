a9488ecfa1c840e939e8fa37465ef7f6da4af566
Raheem Shaik <raheem.shaik@fortunapix.com>
2020-04-20 18:16:53 +0530

    numbering array changed for test

--- bullet.rb
+++ bullet.rb
@@ -1,25 +1,17 @@
+# heads = [
+#     { id: 1, title: "heading1", heading_level: 0 },
+#     { id: 2, title: "heading2", heading_level: 2 },
+#     { id: 3, title: "heading3", heading_level: 1 },
+#     { id: 4, title: "heading4", heading_level: 1 }
+# ]
+
 heads = [
     { id: 1, title: "heading1", heading_level: 0 },
-    { id: 2, title: "heading2", heading_level: 2 },
-    { id: 3, title: "heading3", heading_level: 1 },
-    { id: 4, title: "heading4", heading_level: 1 }
+    { id: 2, title: "heading2", heading_level: 3 },
+    { id: 3, title: "heading3", heading_level: 4 },
+    { id: 4, title: "heading4", heading_level: 1 },
+    { id: 5, title: "heading5", heading_level: 0 }
 ]
-
-# heads = [
-#     { id: 1, title: "heading1", heading_level: 0 },
-#     { id: 2, title: "heading2", heading_level: 3 },
-#     { id: 3, title: "heading3", heading_level: 4 },
-#     { id: 4, title: "heading4", heading_level: 1 },
-#     { id: 5, title: "heading5", heading_level: 0 }
-# ]
-
-# heads = [
-#     { id: 1, title: "heading1", heading_level: 0 },
-#     { id: 2, title: "heading2", heading_level: 3 },
-#     { id: 3, title: "heading3", heading_level: 4 },
-#     { id: 4, title: "heading4", heading_level: 1 },
-#     { id: 5, title: "heading5", heading_level: 0 }
-# ]
 
 heads.each_with_index do |h, i|
     pnt = i > 0 ? heads[0..i-1].select {|p| p[:heading_level] == h[:heading_level] }.count : 0
